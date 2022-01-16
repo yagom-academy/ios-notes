0dc6e5cff148411a656a3a7d78ed88bfba814813
yagom <yagom@yagom.net>
2022-01-17 02:35:44 +0900

    initial commit

---
+++ Notes/Controller/NoteViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Notes - NoteViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class NoteViewController: UIViewController {
+
+    @IBOutlet private weak var noteTextView: UITextView!
+}
---
+++ Notes/View/NoteTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  Notes - NoteTableViewCell.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+final class NoteTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    // MARK: Reuse Identifier
+    static let reuseIdentifier = String(describing: NoteTableViewCell.self)
+    
+    // MARK: IBOutlets
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var shortDescriptionLabel: UILabel!
+    
+    // MARK: - Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+//        clearContents()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        clearContents()
+    }
+    
+    // MARK: Privates
+    private func clearContents() {
+        titleLabel.text = nil
+        dateLabel.text = nil
+        shortDescriptionLabel.text = nil
+    }
+}
