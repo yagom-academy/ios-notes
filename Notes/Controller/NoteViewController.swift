966cf254e1d1883d1008c9acb3b709864518408d
Jinwook Huh <gjwlsdnr0115@gmail.com>
2022-01-19 18:01:26 +0900

    feat: NotesTableViewController, NoteViewController 구현 및 SplitViewController에서 데이터 주고 받기 구현

--- Notes/Controller/NoteViewController.swift
+++ Notes/Controller/NoteViewController.swift
@@ -10,4 +10,43 @@ final class NoteViewController: UIViewController {
     static let storyboardItentifier: String = "NoteVC"
     
     @IBOutlet private weak var noteTextView: UITextView!
+    
+    var noteItem: NoteItem? {
+        didSet {
+            bindNoteItem()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindNoteItem()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        dissmisVCForNilNoteItem()
+    }
+    
+    private func dissmisVCForNilNoteItem() {
+        if noteItem == nil,
+           let splitViewController: UISplitViewController = splitViewController,
+           splitViewController.isCollapsed {
+            let primaryNavController: UINavigationController? = splitViewController.viewControllers.first as? UINavigationController
+            primaryNavController?.popViewController(animated: false)
+        }
+    }
+    
+    private func bindNoteItem() {
+        guard let noteBody = noteItem?.body else {
+            noteTextView.text = ""
+            return
+        }
+        noteTextView.text = noteBody
+    }
+}
+
+extension NoteViewController: NoteSelectionDelegate {
+    func noteSelected(_ selectedNote: NoteItem) {
+        noteItem = selectedNote
+    }
 }
--- Notes/Controller/NotesTableViewController.swift
+++ Notes/Controller/NotesTableViewController.swift
@@ -6,15 +6,26 @@
 
 import UIKit
 
+protocol NoteSelectionDelegate: AnyObject {
+  func noteSelected(_ selectedNote: NoteItem)
+}
+
 final class NotesTableViewController: UITableViewController {
     // MARK: - Properties
-    private var noteItems: [NoteItem] = []
+    var noteItems: [NoteItem] = []
+    
+    weak var delegate: NoteSelectionDelegate?
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         bindNoteItems()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        deselectTableViewRow()
     }
     
     // MARK: - Setup
@@ -27,7 +38,12 @@ final class NotesTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    // MARK: - IBAction Functions
+    // MARK: - Functions
+    private func deselectTableViewRow() {
+        guard let selectedIndex = tableView.indexPathForSelectedRow else { return }
+        tableView.deselectRow(at: selectedIndex, animated: true)
+    }
+    
     @IBAction private func addButtonToggled(_ sender: Any) {
         print("add")
     }
@@ -36,11 +52,10 @@ final class NotesTableViewController: UITableViewController {
 // MARK: - Table view delegate
 extension NotesTableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let noteVC = storyboard?.instantiateViewController(withIdentifier: NoteViewController.storyboardItentifier) as? NoteViewController else {
-            fatalError("No NoteViewController")
+        delegate?.noteSelected(noteItems[indexPath.row])
+        if let noteViewController: NoteViewController = delegate as? NoteViewController {
+            splitViewController?.showDetailViewController(noteViewController, sender: nil)
         }
-        
-        navigationController?.pushViewController(noteVC, animated: true)
     }
 }
 
@@ -58,39 +73,4 @@ extension NotesTableViewController {
         cell.bindCellItem(item: noteItems[indexPath.row])
         return cell
     }
-    
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 }
