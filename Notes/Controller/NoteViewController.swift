5cfc167a1a04114f40d6c1aea50c7e7f668acc30
claire-pp <claire.pp@kakaocorp.com>
2022-01-20 12:08:59 +0900

    feat: [UI] actionSheet 형태만 구현
    메모 뷰에서 더보기 버튼을 눌렀을 때 나오는
    action Sheet 구현

--- Notes/Controller/NoteViewController.swift
+++ Notes/Controller/NoteViewController.swift
@@ -1,14 +1,16 @@
 //
 //  Notes - NoteViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
 final class NoteViewController: UIViewController {
 
     @IBOutlet private weak var noteTextView: UITextView!
+    @IBOutlet weak var additional: UIBarButtonItem!
+    
     private var note: Note?
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,5 +19,19 @@ final class NoteViewController: UIViewController {
     
     func configureView(data: Note) {
         self.note = data
+    }
+    @IBAction func showAdditionalAction(_ sender: Any) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+//        let handler = { (action: UIAlertAction) in print(action.title) }
+//        let deleteAlert = UIAlertAction(title: "진짜요?", style: .destructive, handler: nil)
+        let share = UIAlertAction(title: "share..", style: .default, handler: nil)
+        let delete = UIAlertAction(title: "delete", style: .destructive, handler: nil)
+        let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(share)
+        alertController.addAction(delete)
+        alertController.addAction(cancel)
+        self.present(alertController, animated: true, completion: nil)
+        
     }
 }
